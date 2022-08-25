6ef87aec55ed2af9f46c58de1a6a504b86777172
Jason Little <realtyem@gmail.com>
2022-08-25 02:20:27 -0500

    [CI] Only test stream writers we know function.
    Typing and Receipts appear to be broken in testing.

--- docker/complement/conf/start_for_complement.sh
+++ docker/complement/conf/start_for_complement.sh
@@ -56,13 +56,13 @@ if [[ -n "$SYNAPSE_COMPLEMENT_USE_WORKERS" ]]; then
       federation_inbound, \
       federation_reader, \
       federation_sender, \
+      federation_sender, \
+      synchrotron, \
       synchrotron, \
       appservice, \
       account_data, \
       presence, \
-      receipts, \
       to_device, \
-      typing, \
       pusher"
 
   # Improve startup times by using a launcher based on fork()
