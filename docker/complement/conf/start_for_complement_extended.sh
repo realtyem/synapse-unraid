bcf84665ac1dc08e34885324aab5bade88d07fca
Jason Little <realtyem@gmail.com>
2022-10-02 00:50:27 -0500

    Modify new start_for_complement_extended.sh to not explicitly set SYNAPSE_WORKER_TYPES as we want to pass that in from the CI tests.

--- docker/complement/conf/start_for_complement_extended.sh
+++ docker/complement/conf/start_for_complement_extended.sh
@@ -6,7 +6,9 @@ set -e
 
 echo "Complement Synapse launcher"
 echo "  Args: $@"
-echo "  Env: SYNAPSE_COMPLEMENT_DATABASE=$SYNAPSE_COMPLEMENT_DATABASE SYNAPSE_COMPLEMENT_USE_WORKERS=$SYNAPSE_COMPLEMENT_USE_WORKERS"
+echo "  Env: SYNAPSE_COMPLEMENT_DATABASE=$SYNAPSE_COMPLEMENT_DATABASE"
+echo "  Env: SYNAPSE_COMPLEMENT_USE_WORKERS=$SYNAPSE_COMPLEMENT_USE_WORKERS"
+echo "  Env: SYNAPSE_WORKER_TYPES=$SYNAPSE_WORKER_TYPES"
 
 function log {
     d=$(date +"%Y-%m-%d %H:%M:%S,%3N")
@@ -45,22 +47,23 @@ esac
 
 if [[ -n "$SYNAPSE_COMPLEMENT_USE_WORKERS" ]]; then
   # Specify the workers to test with
-  export SYNAPSE_WORKER_TYPES="\
-      event_persister, \
-      event_persister, \
-      background_worker, \
-      frontend_proxy, \
-      event_creator, \
-      user_dir, \
-      media_repository, \
-      federation_inbound, \
-      federation_reader, \
-      federation_sender, \
-      synchrotron, \
-      appservice, \
-      account_data, \
-      to_device, \
-      pusher"
+  # Actually, this will be calculated and passed in for flexibility.
+  #export SYNAPSE_WORKER_TYPES="\
+  #    event_persister, \
+  #    event_persister, \
+  #    background_worker, \
+  #    frontend_proxy, \
+  #    event_creator, \
+  #    user_dir, \
+  #    media_repository, \
+  #    federation_inbound, \
+  #    federation_reader, \
+  #    federation_sender, \
+  #    synchrotron, \
+  #    appservice, \
+  #    account_data, \
+  #    to_device, \
+  #    pusher"
 
   # Improve startup times by using a launcher based on fork()
   export SYNAPSE_USE_EXPERIMENTAL_FORKING_LAUNCHER=1
