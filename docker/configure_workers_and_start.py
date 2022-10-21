22809a606d6b243116a0fa214b1f5b6686499c9f
Jason Little <realtyem@gmail.com>
2022-10-21 15:51:04 -0500

    Remove no longer needed shared config options 'send_federation' and 'start_pushers' from configure_workers_and_start.py.

--- docker/configure_workers_and_start.py
+++ docker/configure_workers_and_start.py
@@ -65,7 +65,7 @@ WORKERS_CONFIG: Dict[str, Dict[str, Any]] = {
         "app": "synapse.app.generic_worker",
         "listener_resources": [],
         "endpoint_patterns": [],
-        "shared_extra_conf": {"start_pushers": False},
+        "shared_extra_conf": {},
         "worker_extra_conf": "",
     },
     "user_dir": {
@@ -102,7 +102,7 @@ WORKERS_CONFIG: Dict[str, Dict[str, Any]] = {
         "app": "synapse.app.generic_worker",
         "listener_resources": [],
         "endpoint_patterns": [],
-        "shared_extra_conf": {"send_federation": False},
+        "shared_extra_conf": {},
         "worker_extra_conf": "",
     },
     "synchrotron": {
