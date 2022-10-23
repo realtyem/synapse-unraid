578376c5e50ee64e60210a73dd2acba289ec0ff8
Jason Little <realtyem@gmail.com>
2022-10-23 04:38:27 -0500

    Correct hash to use more than first three octets of an IP address.

--- docker/configure_workers_and_start.py
+++ docker/configure_workers_and_start.py
@@ -283,7 +283,7 @@ upstream {upstream_worker_type} {{
 
 NGINX_UPSTREAM_HASH_BY_CLIENT_IP_CONFIG_BLOCK = """
 upstream {upstream_worker_type} {{
-    ip_hash;
+    hash $http_x_forwarded_for;
 {body}
 }}
 """
