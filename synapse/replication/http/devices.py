2343766063b65d95db7a418382a045abdbab7d5d
Jason Little <realtyem@gmail.com>
2022-11-04 04:42:35 -0500

    Forgot to import parse_json_object_from_request.

--- synapse/replication/http/devices.py
+++ synapse/replication/http/devices.py
@@ -18,6 +18,7 @@ from typing import TYPE_CHECKING, Tuple
 from twisted.web.server import Request
 
 from synapse.http.server import HttpServer
+from synapse.http.servlet import parse_json_object_from_request
 from synapse.replication.http._base import ReplicationEndpoint
 from synapse.types import JsonDict
 
